fc2df4eff4c38b4d2fe36cd3a0da04a528e09695
Rian <rianfreires40@gmail.com>
2026-03-10 16:18:16 -0300

    feat: add parse method to scrapy spider

--- guardianscraper/guardianscraper/spiders/guardian.py
+++ guardianscraper/guardianscraper/spiders/guardian.py
@@ -7,4 +7,22 @@ class GuardianSpider(scrapy.Spider):
     start_urls = ["https://theguardian.com"]
 
     def parse(self, response):
-        pass
+        articles = response.css("div.dcr-mwwxk")
+        for article in articles:
+            title = article.css("span.headline-text::text").get()
+            category = article.css("h3 div::text").get()
+            url = article.css("a.attr(href)").get()
+            time = article.css("time::attr(datetime)").get()
+
+            if(url):
+                url = response.urljoin(url)
+                yield scrapy.Request(
+                    url,
+                    callback = self.parse_article,
+                    meta = {
+                        "title" : title,
+                        "category" : category,
+                        "url" : url,
+                        "time" : time 
+                    }
+                )
