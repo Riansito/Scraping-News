8c9cac4dea2b3cb5a8088629d843026ec9a3724d
Rian <rianfreires40@gmail.com>
2026-03-10 23:28:30 -0300

    feat: implement BigQuery pipeline for storing scraped articles

--- guardianscraper/guardianscraper/pipelines.py
+++ guardianscraper/guardianscraper/pipelines.py
@@ -1,13 +1,34 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from google.cloud import bigquery
 
+class BigQueryPipeline:
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+    def __init__(self):
+        self.client = bigquery.Client.from_service_account_json(
+            "../credentials/credentials.json"
+        )
+        self.table_id = "scrapy-489900.scrapydata.articles"
+        self.rows = []
 
-
-class GuardianscraperPipeline:
     def process_item(self, item, spider):
+
+        self.rows.append({
+            "headline": item["headline"],
+            "category": item["category"],
+            "author": item["author"],
+            "published_time": item["published_time"],
+            "article_text": item["article_text"],
+            "url": item["url"]
+        })
+
         return item
+
+    def close_spider(self, spider):
+
+        errors = self.client.load_table_from_json(
+            self.rows,
+            self.table_id
+        )
+
+        errors.result()
+
+        spider.logger.info("Dados enviados para BigQuery")
