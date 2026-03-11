9c78687979e05111cc1287ce6b528c1263534c13
Rian <rianfreires40@gmail.com>
2026-03-11 12:31:32 -0300

    feat: connect FastAPI with BigQuery

--- api/main.py
+++ api/main.py
@@ -1,7 +1,34 @@
 from fastapi import FastAPI
+from google.cloud import bigquery
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return "Hello World"
+client = bigquery.Client.from_service_account_json("../credentials/credentials.json")
+
+TABLE = "scrapy-489900.scrapydata.articles"
+
+app = FastAPI()
+
+@app.get("/search")
+def search_articles():
+
+    query = f"""
+        SELECT headline, author, url, article_text
+        FROM `{TABLE}`
+        LIMIT 5
+    """
+
+    results = client.query(query).result()
+
+    articles = []
+
+    for row in results:
+        articles.append({
+            "headline": row.headline,
+            "author": row.author,
+            "url": row.url,
+            "text": row.article_text,
+            
+        })
+
+    return articles
